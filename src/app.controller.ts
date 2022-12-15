dd7dcb51b9ad54c150c11ee4f91a9ec66b4e3e63
NicolasLima1 <nicolasbasto76@gmail.com>
2022-12-15 20:01:51 -0300

    delete-getUnique

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'node:crypto';
+import { createNotificationBody } from './create-notification-body';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -11,8 +12,17 @@ export class AppController {
     return this.prisma.notification.findMany();
   }
 
+  @Get(":id")
+  async retrieve(id: string) {
+    return await this.prisma.notification.findFirst({
+      where: {
+        id: id
+      }
+    });
+  }
+
   @Post()
-  async create(@Body() body: any) {
+  async create(@Body() body: createNotificationBody) {
     const { recipientId, content, category } = body;
 
     await this.prisma.notification.create({
@@ -23,5 +33,19 @@ export class AppController {
         recipientId,
       },
     });
+  }
+
+  @Delete(":id")
+  async delete(id: string) {
+
+    const deletedNotification = await this.prisma.notification.findFirst({
+      where: {
+        id: id
+      }
+    });
+
+    return {
+      message: "Deleted", deletedNotification
+    }
   }
 }

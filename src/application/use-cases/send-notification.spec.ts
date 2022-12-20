4c4637a73d8936724cfb695f5209cf014a91ff39
NicolasLima1 <nicolasbasto76@gmail.com>
2022-12-20 00:40:35 -0300

    add use-cases & repositories

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,19 @@
+import { SendNotification } from './send-notification';
+import { Notification } from '../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+
+describe('Send notification', () => {
+  it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
+
+    const { notification } = await sendNotification.execute({
+      content: 'This is a notification',
+      category: 'social',
+      recipientId: 'example-recipiente-id',
+    });
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
+  });
+});
